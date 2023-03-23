cc4fb5a2d20a112e5dfac1e58bda0cb209e38cc5
johnbobelisa <johnelisaaa@gmail.com>
2023-03-23 15:27:13 +1100

    Update layer_store.py
    -

--- layer_store.py
+++ layer_store.py
@@ -7,6 +7,7 @@ from queue_adt import *
 from stack_adt import *
 from array_sorted_list import *
 from sorted_list_adt import *
+from data_structures.abstract_list import *
 
 class LayerStore(ABC):
 
@@ -108,21 +109,21 @@ class AdditiveLayerStore(LayerStore):
             self.current_color = start
             return start
 
-        elif self.current_color == None:
-            for _  in range(self.current_layers.length):
-                new_layer = self.current_layers.serve()
-                self.current_layers.append(new_layer)
-                served_layer = new_layer.apply(start, timestamp , x, y)
-                self.current_color = served_layer
-            return self.current_color
-
         else:
             for _ in range(self.current_layers.length):
-                new_layer:Layer = self.current_layers.serve()
-                self.current_layers.append(new_layer)
-                served_layer = new_layer.apply(self.current_color, timestamp, x, y)
-                self.current_color = served_layer
+                if self.current_color == None:
+                    new_layer = self.current_layers.serve()
+                    self.current_layers.append(new_layer)
+                    served_layer = new_layer.apply(start, timestamp , x, y)
+                    self.current_color = served_layer
+                else:
+                    new_layer = self.current_layers.serve()
+                    self.current_layers.append(new_layer)
+                    served_layer = new_layer.apply(self.current_color, timestamp, x, y)
+                    self.current_color = served_layer
+
             return self.current_color
+
 
     def add(self, layer: Layer) -> bool:
         self.current_layers.append(layer)
@@ -156,12 +157,16 @@ class SequenceLayerStore(LayerStore):
         Of all currently applied layers, remove the one with median `name`.
         In the event of two layers being the median names, pick the lexicographically smaller one.
     """
-
+    
     current_layers = ArraySortedList(1)
+    special_layers = ArraySortedList(1)
+    current_layers.reset
     current_color = None
 
     def __init__(self) -> None:
         super().__init__()
+
+    
 
     def get_color(self, start, timestamp, x, y) -> tuple[int, int, int]:
         if self.current_layers.is_empty():
@@ -170,53 +175,53 @@ class SequenceLayerStore(LayerStore):
         
         else:
             for layer in self.current_layers:
-                if layer.applying == True:
-                    layer_value = layer.value
-                    if self.current_color == None:
-                        new_layer = layer_value.apply(start, timestamp, x, y)
-                        self.current_color = new_layer
-                    
-                    else:
-                        new_layer = layer_value.apply(self.current_color, timestamp, x, y)
-                        self.current_color = new_layer
+                layer_value = layer.value
+                if self.current_color == None:
+                    new_layer = layer_value.apply(start, timestamp, x, y)
+                    self.current_color = new_layer
                 
-                continue
-                        
+                else:
+                    new_layer = layer_value.apply(self.current_color, timestamp, x, y)
+                    self.current_color = new_layer
+                    
             return self.current_color
             
                 
     def add(self, layer: Layer) -> bool:
         element = ListItem(layer, layer.index)
-        element.applying = True
-        self.current_layers.add(element)
+        if self.current_layers.is_full():
+            self.current_layers._resize()
+            self.current_layers.add(element)
+        else:
+            self.current_layers.add(element)
         return True
 
     def erase(self, layer: Layer) -> bool:
         element = ListItem(layer, layer.index)
-        for e in self.current_layers:
-            if e == element:
-                e.applying = False
-                break
-            continue
+        e_index = self.current_layers.index(element)
+        self.current_layers.delete_at_index(e_index-2)
         return True
     
     def special(self):
-        pass
-    
-        
-    # def index_sort(lst:ArraySortedList(ListItem)):
-    #     n = len(lst)
-    #     for i in range(n):
-    #         for j in range(n-i-1):
-    #             if lst[j].key > lst[j+1].key:
-    #                 lst[j], lst[j+1] = lst[j+1], lst[j]
-    #     return lst
-    
-    
-    # def alphabetical_sort(lst):
-    #     n = len(lst)
-    #     for i in range(n):
-    #         for j in range(n-i-1):
-    #             if lst[j] > lst[j+1]:
-    #                 lst[j], lst[j+1] = lst[j+1], lst[j]
-    #     return lst
+        #black, blue, darken, green, invert, lighten, rainbow, red, sparkle
+        item1 = ListItem(black, 1)
+        item2 = ListItem(blue, 2)
+        item3 = ListItem(darken, 3)
+        item4 = ListItem(green, 4)
+        item5 = ListItem(invert, 5)
+        item6 = ListItem(lighten, 6)
+        item7 = ListItem(rainbow, 7)
+        item8 = ListItem(red, 8)
+        item9 = ListItem(sparkle, 9)
+
+        elements = 1,2,3,4,5,6,7,8,9
+
+
+    def alphabetical_sort(lst:ArrayStack):
+        n = lst.length
+        for i in range(n):
+            for j in range(n-i-1):
+                if lst[j] > lst[j+1]:
+                    lst[j], lst[j+1] = lst[j+1], lst[j]
+        return lst
+   
